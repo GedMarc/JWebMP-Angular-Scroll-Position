1b1c2e8d6f9b2bf3a2c04905ed4148a8af87f25f
Marc Magon <ged_marc@hotmail.com>
2017-06-26 20:32:25 +0200

    Finalize the CRP Implementation and set to forced

--- src/za/co/mmagon/jwebswing/plugins/angularscrollposition/AngularScrollPositionDirective.java
+++ src/za/co/mmagon/jwebswing/plugins/angularscrollposition/AngularScrollPositionDirective.java
@@ -8,7 +8,7 @@ import za.co.mmagon.jwebswing.base.angular.directives.AngularDirectiveBase;
  * @author Marc Magon
  * @since 30 May 2017
  */
-public class AngularScrollPositionDirective extends AngularDirectiveBase
+class AngularScrollPositionDirective extends AngularDirectiveBase
 {
 
     private static final long serialVersionUID = 1L;
