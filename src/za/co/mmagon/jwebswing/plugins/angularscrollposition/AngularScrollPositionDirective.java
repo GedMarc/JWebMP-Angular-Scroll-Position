694fd80c6a248ecfd437920c94d05f51d55bed3d
Marc Magon <ged_marc@hotmail.com>
2017-08-13 01:07:43 +0200

    0.11.4.1 Updates.Global Fixes.

--- src/za/co/mmagon/jwebswing/plugins/angularscrollposition/AngularScrollPositionDirective.java
+++ src/za/co/mmagon/jwebswing/plugins/angularscrollposition/AngularScrollPositionDirective.java
@@ -4,26 +4,25 @@ import za.co.mmagon.FileTemplates;
 import za.co.mmagon.jwebswing.base.angular.directives.AngularDirectiveBase;
 
 /**
- *
  * @author Marc Magon
  * @since 30 May 2017
  */
 class AngularScrollPositionDirective extends AngularDirectiveBase
 {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    /*
-     * Constructs a new AngularScrollPositionDirective
-     */
-    public AngularScrollPositionDirective()
-    {
-        super("AngularScrollPositionDirective");
-    }
+	/*
+	 * Constructs a new AngularScrollPositionDirective
+	 */
+	public AngularScrollPositionDirective()
+	{
+		super("AngularScrollPositionDirective");
+	}
 
-    @Override
-    public String renderFunction()
-    {
-        return FileTemplates.getFileTemplate(AngularScrollPositionDirective.class, "scrollPosition.min").toString();
-    }
+	@Override
+	public String renderFunction()
+	{
+		return FileTemplates.getFileTemplate(AngularScrollPositionDirective.class, "scrollPosition.min").toString();
+	}
 }
--- src/za/co/mmagon/jwebswing/plugins/angularscrollposition/AngularScrollPositionPageConfigurator.java
+++ src/za/co/mmagon/jwebswing/plugins/angularscrollposition/AngularScrollPositionPageConfigurator.java
@@ -14,44 +14,44 @@ import za.co.mmagon.jwebswing.plugins.jquery.JQueryPageConfigurator;
  * @since 16 Jun 2017
  */
 @PluginInformation(pluginName = "Angular Scroll Position",
-        pluginDescription = "Allows to set the scroll position via angular",
-        pluginUniqueName = "jwebswing-angular-scroll-position",
-        pluginVersion = "1.0.0",
-        pluginCategories = "angular,animation,zoom, ui,web ui, framework",
-        pluginSubtitle = "Allows for almost anything to have the zoom in effect attached by simply adding the zoomin class to any element.",
-        pluginSourceUrl = "https://www.akveo.com/products.html",
-        pluginWikiUrl = "https://github.com/GedMarc/JWebSwing/Angular-Scroll-Position/wiki",
-        pluginGitUrl = "https://github.com/GedMarc/JWebSwing/Angular-Scroll-Position",
-        pluginIconUrl = "",
-        pluginIconImageUrl = "",
-        pluginOriginalHomepage = "https://www.akveo.com/products.html",
-        pluginDownloadUrl = "https://sourceforge.net/projects/jwebswing/files/plugins/AngularZoomInAnimation.jar/download"
+		pluginDescription = "Allows to set the scroll position via angular",
+		pluginUniqueName = "jwebswing-angular-scroll-position",
+		pluginVersion = "1.0.0",
+		pluginCategories = "angular,animation,zoom, ui,web ui, framework",
+		pluginSubtitle = "Allows for almost anything to have the zoom in effect attached by simply adding the zoomin class to any element.",
+		pluginSourceUrl = "https://www.akveo.com/products.html",
+		pluginWikiUrl = "https://github.com/GedMarc/JWebSwing/Angular-Scroll-Position/wiki",
+		pluginGitUrl = "https://github.com/GedMarc/JWebSwing/Angular-Scroll-Position",
+		pluginIconUrl = "",
+		pluginIconImageUrl = "",
+		pluginOriginalHomepage = "https://www.akveo.com/products.html",
+		pluginDownloadUrl = "https://sourceforge.net/projects/jwebswing/files/plugins/AngularZoomInAnimation.jar/download"
 )
 @ComponentInformation(name = "Angular Zoom In",
-        description = "Allows for almost anything to have the zoom in effect attached by simply adding the zoomin class to any element.",
-        url = "https://www.akveo.com/products.html")
+		description = "Allows for almost anything to have the zoom in effect attached by simply adding the zoomin class to any element.",
+		url = "https://www.akveo.com/products.html")
 public class AngularScrollPositionPageConfigurator extends PageConfigurator
 {
-
-    private static final long serialVersionUID = 1L;
-
-    /*
-     * Constructs a new AngularScrollPositionPageConfigurator
-     */
-    public AngularScrollPositionPageConfigurator()
-    {
-        //Nothing needed
-    }
-
-    @Override
-    public Page configure(Page page)
-    {
-        if (!page.isConfigured())
-        {
-            JQueryPageConfigurator.setRequired(page.getBody(), true);
-            AngularPageConfigurator.setRequired(page.getBody(), true);
-            page.getAngular().getAngularDirectives().add(new AngularScrollPositionDirective());
-        }
-        return page;
-    }
+	
+	private static final long serialVersionUID = 1L;
+	
+	/*
+	 * Constructs a new AngularScrollPositionPageConfigurator
+	 */
+	public AngularScrollPositionPageConfigurator()
+	{
+		//Nothing needed
+	}
+	
+	@Override
+	public Page configure(Page page)
+	{
+		if (!page.isConfigured())
+		{
+			JQueryPageConfigurator.setRequired(page.getBody(), true);
+			AngularPageConfigurator.setRequired(page.getBody(), true);
+			page.getAngular().getAngularDirectives().add(new AngularScrollPositionDirective());
+		}
+		return page;
+	}
 }
