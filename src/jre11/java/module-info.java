5afcdd9c77049448508041341b0a704eb55f4d5b
GedMarc <ged_marc@hotmail.com>
2020-11-13 00:38:25 +0200

    module name java.validation to jakarta.validation

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -2,7 +2,7 @@ module com.jwebmp.plugins.angularscrollposition {
 	exports com.jwebmp.plugins.angularscrollposition;
 
 	requires com.jwebmp.core;
-	requires java.validation;
+	requires jakarta.validation;
 	requires com.guicedee.guicedinjection;
 	requires com.jwebmp.core.angularjs;
 
